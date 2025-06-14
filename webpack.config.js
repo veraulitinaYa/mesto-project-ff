d2ae287c99217fdb8c84479e3478a129efb12de9
veraulitinaYa <veraulitina@yandex.ru>
2025-06-14 12:43:23 +0300

    babel installation and setup done

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,26 @@ module.exports = {
     filename: "main.js",
     publicPath: "",
   },
-mode: 'development',
+  mode: "development",
   devServer: {
-    static: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
 
-    open: true
+    open: true,
+  },
+  module: {
+    rules: [
+      // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: "babel-loader",
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: "/node_modules/",
+      },
+    ],
   },
 };
