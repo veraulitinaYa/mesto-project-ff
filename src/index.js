934b4913f6e9d9084bdee8101f8de5a9986d9b95
veraulitinaYa <veraulitina@yandex.ru>
2025-06-20 17:34:07 +0300

    feature: new card form

--- src/index.js
+++ src/index.js
@@ -10,6 +10,76 @@ const windowNewCard = document.querySelector('.popup_type_new-card');
 
 const windowImage = document.querySelector('.popup_type_image');
 
+const formProfile = document.querySelector('.form__profile');
+const formCard = document.querySelector('.form__card');
+const personNameInput = document.querySelector('.popup__input_type_name');
+const personJobInput = document.querySelector('.popup__input_type_description');
+const cardNameInput = document.querySelector('.popup__input_type_card-name');
+const cardImageLinkInput = document.querySelector('.popup__input_type_url');
+
+
+
+
+function handleFormProfileSubmit(evt) {
+    evt.preventDefault();
+
+//     const saveButton = windowEditPopup.querySelector('.popup__button');
+// saveButton.addEventListener('click', function(){
+// windowEditPopup.classList.remove('popup_is-opened');
+// });
+
+    const jobInputValue = personJobInput.value;
+    const nameInputValue = personNameInput.value;
+
+    const jobProfileInfo = document.querySelector('.profile__description');
+    const nameProfileInfo = document.querySelector('.profile__title');
+
+
+    jobProfileInfo.textContent = jobInputValue;
+    nameProfileInfo.textContent = nameInputValue;
+
+      windowEditPopup.classList.remove('popup_is-opened');
+      formProfile.reset();
+
+
+}
+
+formProfile.addEventListener('submit', handleFormProfileSubmit);
+
+//------------------------------------------------------------------------------------------------------------
+
+
+
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+
+
+
+  const name = cardNameInput.value;
+  const link = cardImageLinkInput.value;
+
+  // Создаем новую карточку и добавляем в начало контейнера
+  const newCard = createCard(name, link, deleteCard);
+
+
+ placesList.prepend(newCard);
+
+
+
+windowNewCard.classList.remove('popup_is-opened');
+  // Закрываем попап и очищаем форму
+
+
+
+  formCard.reset();
+}
+
+// Добавляем обработчик события submit
+formCard.addEventListener('submit', handleCardFormSubmit);
+
+//---------------------------------------------------------------------------------------------------------------
+
 
 buttonEditPopup.addEventListener ('click', function(){
 openPopup(windowEditPopup);
@@ -28,18 +98,19 @@ popup.classList.add('popup_is-opened');
 }
 
 function closePopup (popup) {
+
 const closeButton = popup.querySelector('.popup__close');
 closeButton.addEventListener('click', function(){
 popup.classList.remove('popup_is-opened');
 });
+
+
 document.addEventListener('keydown', function(evt){
   if (evt.key==='Escape'){
     popup.classList.remove('popup_is-opened');
 
-  }
+  }});
 
-
-});
 
 popup.addEventListener('click', function(evt){
   if ((evt.target ===popup)){
@@ -47,7 +118,22 @@ popup.addEventListener('click', function(evt){
   }
 });
 
+
+// button.addEventListener('click', function(){
+// popup.classList.remove('popup_is-opened');
+// });
+
+
+
+
+
+
+
 }
+
+
+
+
 
 const placesList = document.querySelector(".places__list");
 
