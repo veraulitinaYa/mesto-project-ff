f47884d758273cf683dd2dbdf5ec7b42d55accc7
veraulitinaYa <veraulitina@yandex.ru>
2025-07-07 22:47:26 +0300

    Forms: check validity

--- src/index.js
+++ src/index.js
@@ -28,6 +28,116 @@ const personJobInput = document.querySelector(".popup__input_type_description");
 const jobProfileInfo = document.querySelector(".profile__description");
 const nameProfileInfo = document.querySelector(".profile__title");
 
+//-----------------------------------------------showInputError
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input_error_text');
+};
+
+//-------------------------------hideInputError
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_error');
+  errorElement.classList.remove('popup__input_error_text');
+  errorElement.textContent = '';
+};
+//-----------------------------toggleButton
+
+const toggleButtonState = (inputList, buttonElement) => {
+  // Если есть хотя бы один невалидный инпут
+  
+  if (hasInvalidInput(inputList)) {
+    // сделай кнопку неактивной
+        buttonElement.classList.add('popup__button-inactive')
+  } else {
+        // иначе сделай кнопку активной
+    buttonElement.classList.remove('popup__button-inactive');
+  }
+};
+
+
+//------------------------------------------------- check Input Validity
+const checkInputValidity = (formElement, inputElement) => {
+  inputElement.setCustomValidity("");
+
+
+
+  if (inputElement.pattern && (inputElement.type !== 'url')) {
+     const regex = new RegExp(inputElement.pattern);
+    if (!regex.test(inputElement.value)) {
+      inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+    }
+ }
+
+
+
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+//-----------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------------------
+//--------------------------------------- set event listeners
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__button');
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+//----------------------------------------------------------------- enable validation and create fieldList
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+
+    });
+const fieldsetList = Array.from(formElement.querySelectorAll('.popup__input'));
+
+fieldsetList.forEach((fieldSet) => {
+  setEventListeners(formElement);
+});
+
+  });
+};
+
+//-----------------------------------------------------------
+
+enableValidation();
+
+//----------------------------------------------------------------- has invalid input
+
+
+function hasInvalidInput (inputList) {
+  // проходим по этому массиву методом some
+  return inputList.some((inputElement) => {
+        // Если поле не валидно, колбэк вернёт true
+    // Обход массива прекратится и вся функция
+    // hasInvalidInput вернёт true
+
+    return !inputElement.validity.valid;
+  })
+};
+
+//----------------------------------------------------------------------------------------------
+
+
 addCloseButtonListener(windowEditPopup);
 addCloseButtonListener(windowImage);
 addCloseButtonListener(windowNewCard);
